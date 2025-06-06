6ba153ac09d537567137c49b21a627c4ab70e038
Dozhin.V <dozhin.v@gmail.com>
2025-06-06 21:07:21 +0300

    feat: add wasm build to github pages

---
+++ out/bevy_game_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf1653561c61289ca: (a: number, b: number, c: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd21f295b0418077d: (a: number, b: number) => void;
+export const closure5087_externref_shim: (a: number, b: number, c: any) => void;
+export const closure5090_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7fb3c7479fc152dc: (a: number, b: number) => void;
+export const closure118564_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
